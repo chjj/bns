05a2dcc3b1f56b00dd18d7cd0b4a834a7fdf73f9
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-04-06 16:49:14 -0700

    dig: lint.

--- bin/dig.js
+++ bin/dig.js
@@ -258,8 +258,10 @@ function printHeader(host) {
       printHeader(host);
       process.stdout.write(';; Got answer:\n');
       // Note: should go after header flags.
-      if (rd && !res.ra)
-        process.stdout.write(';; WARNING: recursion requested but not available\n');
+      if (rd && !res.ra) {
+        process.stdout.write(';; WARNING:');
+        process.stdout.write(' recursion requested but not available\n');
+      }
       process.stdout.write(res.toString(now, host, port) + '\n');
     }
   }
