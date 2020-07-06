fe9383b031847a72d1c938f4b82f8e02c5c0a615
Matthew Zipkin <pinheadmz@gmail.com>
2020-07-06 16:30:55 -0400

    zone: return CNAME records for any type requested

--- test/zone-test.js
+++ test/zone-test.js
@@ -52,4 +52,158 @@ describe('Zone', function() {
       assert.deepStrictEqual(msg.authority, expect);
     }
   });
+
+  describe('Serve records from zone', function() {
+    const zone = new Zone();
+    const domain = 'thebnszone.';
+    const subdomainWithGlue = 'subdomain-glue.' + domain;
+    const subdomainNoGlue = 'subdomain-external.' + domain;
+    const subdomainWithText = 'subdomain-text.' + domain;
+
+    // TLD
+    zone.setOrigin(domain);
+    // A record for TLD (Common in Handshake, not in DNS)
+    zone.fromString(`${domain} 21600 IN A 10.20.30.40`);
+    // TXT record for TLD
+    zone.fromString(`${subdomainWithText} 21600 IN TXT "subdomain-with-text"`);
+    // TXT for wildcard
+    zone.fromString('* 21600 IN TXT "wildcard"');
+    // CNAME for subdomain -> TLD
+    zone.fromString(`${subdomainWithGlue} 21600 IN CNAME ${domain}`);
+    // CNAME for subdomain -> other zone
+    zone.fromString(`${subdomainNoGlue} 21600 IN CNAME idontexist.`);
+    // SOA to trigger authority flag
+    zone.fromString(
+      `${domain} 21600 IN SOA ns1.${domain} admin.${domain} ` +
+      '2020070500 86400 7200 604800 300'
+    );
+
+    it('should serve A record', () => {
+      const msg = zone.resolve(domain, types.A);
+      assert(msg.code === codes.NOERROR);
+      assert(msg.aa);
+      assert(msg.authority.length === 0);
+      assert(msg.additional.length === 0);
+      assert(msg.answer.length === 1);
+      assert(msg.answer[0].data.address = '10.20.30.40');
+    });
+
+    it('should serve SOA record for missing type', () => {
+      const msg = zone.resolve(domain, types.AAAA);
+      assert(msg.code === codes.NOERROR);
+      assert(msg.aa);
+      assert(msg.authority.length === 1);
+      assert(msg.additional.length === 0);
+      assert(msg.answer.length === 0);
+    });
+
+    it('should serve TXT record for wildcard', () => {
+      const msg = zone.resolve(`idontexist.${domain}`, types.TXT);
+      assert(msg.code === codes.NOERROR);
+      assert(!msg.aa);
+      assert(msg.authority.length === 0);
+      assert(msg.additional.length === 0);
+      assert(msg.answer.length === 1);
+      assert(msg.answer[0].data.txt.length === 1);
+      assert(msg.answer[0].data.txt[0] === 'wildcard' );
+    });
+
+    it('should serve TXT record for defined subdomain', () => {
+      const msg = zone.resolve(`${subdomainWithText}`, types.TXT);
+      assert(msg.code === codes.NOERROR);
+      assert(!msg.aa);
+      assert(msg.authority.length === 0);
+      assert(msg.additional.length === 0);
+      assert(msg.answer.length === 1);
+      assert(msg.answer[0].data.txt.length === 1);
+      assert(msg.answer[0].data.txt[0] === 'subdomain-with-text');
+    });
+
+    for (const t of Object.keys(types)) {
+      it(`should serve CNAME + glue as answers for type: ${t}`, () => {
+        if (t === 'NS' || t === 'ANY')
+          this.skip(); // TODO
+
+        const msg = zone.resolve(subdomainWithGlue, types[t]);
+        assert(msg.code === codes.NOERROR);
+        assert(!msg.aa);
+        assert(msg.authority.length === 0);
+        assert(msg.additional.length === 0);
+        assert(msg.answer.length === 2);
+
+        let cname = false;
+        let a = false;
+        for (const an of msg.answer) {
+          if (an.type === types.CNAME)
+            cname = true;
+
+          if (an.type === types.A) {
+            a = true;
+            assert (an.data.address = '10.20.30.40');
+          }
+        }
+        assert(cname);
+        assert(a);
+      });
+    }
+
+    for (const t of Object.keys(types)) {
+      it(`should serve CNAME only for type: ${t}`, () => {
+        if (t === 'NS' || t === 'ANY')
+          this.skip(); // TODO
+
+        const msg = zone.resolve(subdomainNoGlue, types[t]);
+        assert(msg.code === codes.NOERROR);
+        assert(!msg.aa);
+        assert(msg.authority.length === 0);
+        assert(msg.additional.length === 0);
+        assert(msg.answer.length === 1);
+        assert(msg.answer[0].type = types.CNAME);
+        assert(msg.answer[0].data.target = 'idontexist.');
+      });
+    }
+  });
+
+  describe('CNAME for wildcard', function() {
+    const zone = new Zone();
+    const domain = 'thebnszone.';
+    const subdomainWithGlue = 'subdomain-glue.' + domain;
+
+    // TLD
+    zone.setOrigin(domain);
+    // Reset zone.
+    zone.clearRecords();
+    // A record for TLD (Common in Handshake, not in DNS)
+    zone.fromString(`${domain} 21600 IN A 10.20.30.40`);
+    // CNAME for wildcard -> TXT
+    zone.fromString(`* 21600 IN CNAME ${domain}`);
+
+    for (const t of Object.keys(types)) {
+      it(`should serve CNAME + glue as answers for type: ${t}`, () => {
+        if (t === 'NS' || t === 'ANY')
+          this.skip(); // TODO
+
+        const msg = zone.resolve(subdomainWithGlue, types[t]);
+        assert(msg.code === codes.NOERROR);
+        assert(!msg.aa);
+        assert(msg.authority.length === 0);
+        assert(msg.additional.length === 0);
+        assert(msg.answer.length === 2);
+
+        let cname = false;
+        let a = false;
+        for (const an of msg.answer) {
+          if (an.type === types.CNAME)
+            cname = true;
+
+          if (an.type === types.A) {
+            a = true;
+            assert (an.data.address = '10.20.30.40');
+          }
+        }
+        assert(cname);
+        assert(a);
+      });
+    }
+  });
 });
